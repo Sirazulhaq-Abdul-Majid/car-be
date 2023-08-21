b2b58381dbe114389c2f2fe0bf8640a5828f27c9
Zul <sirazulhaq0205@gmail.com>
2023-08-22 00:09:22 +0800

    added sanitization

--- src/users/dto/signup.dto.ts
+++ src/users/dto/signup.dto.ts
@@ -1,17 +1,31 @@
-import { IsAlphanumeric, IsEmail, IsString, MinLength } from "class-validator"
+import { Escape } from "class-sanitizer"
+import { Transform, TransformFnParams } from "class-transformer"
+import { IsAlphanumeric, IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator"
+import * as sanitizeHtml from 'sanitize-html'
 
 export class SignupDTO {
 
   @IsString()
+  @IsNotEmpty()
+  @Escape()
+  @Transform((params: TransformFnParams) => sanitizeHtml(params.value))
   full_name: string
 
   @IsEmail()
+  @IsNotEmpty()
+  @Escape()
+  @Transform((params: TransformFnParams) => sanitizeHtml(params.value))
   email: string
 
-  @IsAlphanumeric()
   @MinLength(8)
+  @IsNotEmpty()
+  @Escape()
+  @Transform((params: TransformFnParams) => sanitizeHtml(params.value))
   password: string
 
   @IsAlphanumeric()
+  @IsNotEmpty()
+  @Escape()
+  @Transform((params: TransformFnParams) => sanitizeHtml(params.value))
   login_id: string
 }
