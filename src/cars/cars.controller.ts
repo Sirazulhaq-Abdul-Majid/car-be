6611b9db13914fff2eb750ac08e5632f3c82bcd0
Zul <sirazulhaq0205@gmail.com>
2023-09-03 21:30:12 +0800

    remove auth

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -11,7 +11,6 @@ import { AddCarDTO, SearchCarDTO } from './dto';
 export class CarsController {
   constructor(private carsService: CarsService) { }
 
-  @UseGuards(AccessTokenGuard)
   @Post('save')
   @UseInterceptors(FilesInterceptor('image', 20, multerConfig))
   async addCar(@Body() carDto: AddCarDTO, @Request() req: any, @UploadedFiles() files: Array<Express.Multer.File>) {
